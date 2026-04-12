2660f9cf87b39922f2c04cf26a323b20aa9c0378
zhn2605 <zzihan0805@gmail.com>
2026-04-12 16:06:03 -0400

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-center mt-10">Alice</h1>
+    </main>
+  );
+}
